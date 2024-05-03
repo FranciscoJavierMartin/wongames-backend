8c54579890774a34be063ea5834101a4b052b959
Francisco <javiermartincuenca@gmail.com>
2024-05-03 06:48:41 +0200

    Create project

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GameModule } from './game/game.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    GameModule,
+  ],
+})
+export class AppModule {}
