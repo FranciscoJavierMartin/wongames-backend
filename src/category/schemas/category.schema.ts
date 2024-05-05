9daf6fdccfc4bb047e194858d811e87dc2304a5e
Francisco <javiermartincuenca@gmail.com>
2024-05-05 21:37:53 +0200

    WIP

--- src/category/schemas/category.schema.ts
+++ src/category/schemas/category.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-@Schema()
+@Schema({ toJSON: { virtuals: true } })
 export class Category {
   @Prop({ unique: true, isRequired: true })
   name: string;
--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -4,9 +4,15 @@ import { GameService } from './game.service';
 import { GameResolver } from './game.resolver';
 import { GameController } from './game.controller';
 import { Game, GameSchema } from './schemas/game.shema';
+import { Category, CategorySchema } from 'src/category/schemas/category.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: Game.name, schema: GameSchema}])],
+  imports: [
+    MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+  ],
   providers: [GameResolver, GameService],
   controllers: [GameController],
 })
