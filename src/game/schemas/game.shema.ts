22971eb22f69ba83f63f866feb6bf01d7ff07921
Francisco <javiermartincuenca@gmail.com>
2024-05-06 17:37:24 +0200

    WIP

--- src/game/schemas/game.shema.ts
+++ src/game/schemas/game.shema.ts
@@ -1,13 +1,42 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
+// TODO: Refactor
+export enum Rating {
+  FREE = 'FREE',
+  pegi3 = 'pegi3',
+  pegi7 = 'pegi7',
+  pegi12 = 'pegi12',
+  pegi16 = 'pegi16',
+  pegi18 = 'pegi18',
+}
+
 @Schema()
 export class Game {
-  @Prop()
+  @Prop({ isRequired: true })
   name: string;
 
   @Prop()
   shortDescription: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ isRequired: true })
+  price: number;
+
+  @Prop()
+  releaseDate: Date;
+
+  // TODO: Add proper type
+  @Prop()
+  rating: Rating;
+
+  @Prop()
+  cover: string;
+
+  @Prop()
+  gallery: string[];
 }
 
 export type GameDocument = HydratedDocument<Game>;
