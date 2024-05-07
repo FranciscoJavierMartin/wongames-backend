f6c41910b4e127bb4894232c07bb8a982243bec2
Francisco <javiermartincuenca@gmail.com>
2024-05-07 22:34:46 +0200

    Remove DTOs

--- src/platform/dto/update-platform.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreatePlatformInput } from './create-platform.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdatePlatformInput extends PartialType(CreatePlatformInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/publisher/dto/update-publisher.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreatePublisherInput } from './create-publisher.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdatePublisherInput extends PartialType(CreatePublisherInput) {
-  @Field(() => Int)
-  id: number;
-}
