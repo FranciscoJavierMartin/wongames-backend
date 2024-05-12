4d39b8f1006cb1c72ca6a2d0fde8e3c58bb17a4e
Francisco <javiermartincuenca@gmail.com>
2024-05-12 12:36:02 +0200

    Install vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    passWithNoTests: true,
+    include: [`tests/units/**/*.test.ts`],
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
